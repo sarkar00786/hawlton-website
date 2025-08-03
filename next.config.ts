254b19ee88b1c5e1ee15c2cc66138d8faeb46a5b
Jhangir Hussain <azkabloch786@gmail.com>
2025-08-03 12:19:40 +0500

    Update CSP to allow Google authentication domains - add accounts.google.com, oauth2.googleapis.com, securetoken.googleapis.com, identitytoolkit.googleapis.com

--- next.config.ts
+++ next.config.ts
@@ -49,7 +49,7 @@ const nextConfig: NextConfig = {
           style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://fonts.gstatic.com;
           img-src 'self' data: blob: https://*.sanity.io https://*.googletagmanager.com https://*.google-analytics.com https://*.googleadservices.com https: https://*.gstatic.com;
           font-src 'self' https://fonts.gstatic.com https://fonts.googleapis.com data:;
-          connect-src 'self' https://*.google-analytics.com https://*.googletagmanager.com https://*.googleadservices.com https://*.sanity.io https://fonts.googleapis.com https://fonts.gstatic.com https://vercel.live wss://ws-us3.pusher.com https://api.hawlton.com https://firestore.googleapis.com https://*.firebaseapp.com https://*.googleapis.com https://www.google.com data: blob:;
+          connect-src 'self' https://*.google-analytics.com https://*.googletagmanager.com https://*.googleadservices.com https://*.sanity.io https://fonts.googleapis.com https://fonts.gstatic.com https://vercel.live wss://ws-us3.pusher.com https://api.hawlton.com https://firestore.googleapis.com https://*.firebaseapp.com https://*.googleapis.com https://www.google.com https://accounts.google.com https://oauth2.googleapis.com https://securetoken.googleapis.com https://identitytoolkit.googleapis.com data: blob:;
           frame-src 'self' https://*.tawk.to https://vercel.live https://www.google.com;
           worker-src 'self' blob:;
           child-src 'self' blob:;
